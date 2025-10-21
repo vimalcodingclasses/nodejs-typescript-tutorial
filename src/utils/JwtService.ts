8fefc6b06705acb95d3936003e463a8dcd179628
vimalcodingclasses <vimalcodingclasses@gmail.com>
2025-10-21 18:21:17 +0530

    using RS256 algorithm in jwt

--- src/utils/JwtService.ts
+++ src/utils/JwtService.ts
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import fs from 'fs';
 
 export class JwtService {
     private expiresIn: number;
@@ -10,10 +11,12 @@ export class JwtService {
     }
 
     public generateToken(payload: any): string {
-        return jwt.sign({ data: payload }, this.secret, { expiresIn: this.expiresIn })
+        const privateKey = fs.readFileSync(process.env.PRIVATE_KEY_PATH!);
+        return jwt.sign({ data: payload }, privateKey, { algorithm: 'RS256', expiresIn: this.expiresIn })
     }
 
     public verifyToken(token: string): any {
-        return jwt.verify(token, this.secret);
+        const cert = fs.readFileSync(process.env.PUBLIC_PEM_PATH!);
+        return jwt.verify(token, cert);
     }
 }
